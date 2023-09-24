3e0832ece489b6de2c4704cce9c993c44e47793b
IlyaKorepanov <ikorepanov@yandex.ru>
2023-09-24 21:09:28 +0300

    ДЗ 4-е задания выполнены

--- task02/Program.cs
+++ task02/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите первое число ");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число ");
+int number2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите третье число ");
+int number3 = int.Parse(Console.ReadLine());
+
+int result = number1;
+
+if (result < number2)
+{
+    result = number2;
+}
+if (result < number3)
+{
+    result = number3;
+}
+
+Console.WriteLine($"Наибольшее число = {result}");
+
--- task04/Program.cs
+++ task04/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число ");
+int number = int.Parse(Console.ReadLine());
+
+int count = 2;
+
+if (number > 1)
+{
+    while (count <= number)
+    {
+        Console.Write($"{count} ");
+        count = count + 2;
+    }
+}
+else if (number <= 1)
+{
+    Console.WriteLine($"Нет решения, число {number} меньше или равно 1");
+}
+Console.WriteLine();
